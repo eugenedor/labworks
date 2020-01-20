0404292ba66883da95b5860b12a77f6a893ae0de
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-01-20 20:40:48 +0300

    Nloguse

---
+++ Nloguse/Nloguse/LoggerHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Nloguse
+{
+    public class LoggerHelper
+    {
+        private static readonly Lazy<LoggerHelper> _instance = new Lazy<LoggerHelper>(() => new LoggerHelper());        
+
+        LoggerHelper()
+        {
+            Log = LogManager.GetLogger(GetType().FullName);
+        }
+
+        public Logger Log { get; private set; }
+
+        public static LoggerHelper Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+    }
+}
