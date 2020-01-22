e3d8cd0939c3906e744ebc28096610a46f6ba3a8
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-01-22 21:58:41 +0300

    select tbl

--- LabClientServerApp/SqlScript/DraftSqlScript/200122_05_insert_tbl.sql
+++ LabClientServerApp/SqlScript/DraftSqlScript/200122_05_insert_tbl.sql
@@ -1,14 +1,27 @@
 USE RedBook
 GO
 
-UPDATE dbo.CT_Squads
+UPDATE CT_Squads
 SET SquadName = IM_Squads.SquadName
 FROM IM_Squads
 WHERE CT_Squads.SquadCode = IM_Squads.SquadCode;
 
-INSERT INTO dbo.CT_Squads (SquadCode, SquadName)
+INSERT INTO CT_Squads (SquadCode, SquadName)
 SELECT im.SquadCode, im.SquadName
-FROM dbo.IM_Squads im
-     LEFT JOIN dbo.CT_Squads ct
+FROM IM_Squads im
+     LEFT JOIN CT_Squads ct
 	   ON im.SquadCode = ct.SquadCode
 WHERE ct.SquadId IS NULL;
+
+
+UPDATE CT_Types
+SET TypeName = (SELECT TypeName 
+                FROM IM_Types 
+				WHERE CT_Types.TypeCode = IM_Types.TypeCode);
+
+INSERT INTO CT_Types (TypeCode, TypeName)
+SELECT im.TypeCode, im.TypeName
+FROM IM_Types im
+     LEFT JOIN CT_Types ct
+	   ON im.TypeCode = ct.TypeCode
+WHERE ct.TypeId IS NULL;
