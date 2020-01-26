6690f7da1a3a21f4bab722ebf9454bb6d29b460a
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-01-26 23:28:15 +0300

    SK_Animals

--- LabClientServerApp/SqlScript/DraftSqlScript/200122_05_insert_tbl.sql
+++ LabClientServerApp/SqlScript/DraftSqlScript/200122_05_insert_tbl.sql
@@ -1,6 +1,9 @@
 USE RedBook
 GO
 
+--
+--CT_Squads
+--
 UPDATE CT_Squads
 SET SquadName = IM_Squads.SquadName
 FROM IM_Squads
@@ -14,6 +17,9 @@ FROM IM_Squads im
 WHERE ct.SquadId IS NULL;
 
 
+--
+--CT_Types
+--
 UPDATE CT_Types
 SET TypeName = (SELECT TypeName 
                 FROM IM_Types 
@@ -25,3 +31,31 @@ FROM IM_Types im
      LEFT JOIN CT_Types ct
 	   ON im.TypeCode = ct.TypeCode
 WHERE ct.TypeId IS NULL;
+
+
+--
+--SK_Animals
+--
+UPDATE SK_Animals
+SET AnimalName = IM_Animals.AnimalName,
+    SquadId = CT_Squads.SquadId,
+	TypeId = CT_Types.TypeId,
+	TypicalWeight = IM_Animals.TypicalWeight
+FROM SK_Animals
+     JOIN IM_Animals
+	   ON SK_Animals.AnimalCode = IM_Animals.AnimalCode
+     LEFT JOIN CT_Squads
+	   ON IM_Animals.SquadCode = CT_Squads.SquadCode
+	 LEFT JOIN CT_Types
+	   ON IM_Animals.TypeCode = CT_Types.TypeCode;
+
+INSERT INTO SK_Animals (AnimalCode, AnimalName, SquadId, TypeId, TypicalWeight)
+SELECT IM_Animals.AnimalCode, IM_Animals.AnimalName, CT_Squads.SquadId, CT_Types.TypeId, IM_Animals.TypicalWeight
+FROM IM_Animals 
+     LEFT JOIN SK_Animals
+	   ON IM_Animals.AnimalCode = SK_Animals.AnimalCode
+     LEFT JOIN CT_Squads
+	   ON IM_Animals.SquadCode = CT_Squads.SquadCode
+	 LEFT JOIN CT_Types
+	   ON IM_Animals.TypeCode = CT_Types.TypeCode
+WHERE SK_Animals.AnimalId IS NULL;
