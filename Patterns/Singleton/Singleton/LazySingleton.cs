3d3dbdaf221e966d5d19defb0a53517258ef622b
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-02-03 15:19:18 +0300

    Patterns Singleton

---
+++ Patterns/Singleton/Singleton/LazySingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Singleton
+{
+    public sealed class LazySingleton
+    {
+        private static readonly Lazy<LazySingleton> _instance = new Lazy<LazySingleton>(() => new LazySingleton());
+
+        LazySingleton()
+        {
+            Log = LogManager.GetLogger(GetType().FullName);
+        }
+
+        public Logger Log { get; private set; }
+
+        public static LazySingleton Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+    }
+}
---
+++ Patterns/Singleton/Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                LazySingleton.Instance.Log.Info("info message start");
+
+                var text = Console.ReadLine();                
+                LazySingleton.Instance.Log.Info($"{text}");
+                Console.ReadKey();
+
+                //Logger log = LogManager.GetCurrentClassLogger();
+                //log.Info("info message");
+            }
+            catch (Exception ex)
+            {
+                LazySingleton.Instance.Log.Info($"{ex.Message}");
+            }
+            finally
+            {
+                LazySingleton.Instance.Log.Info("info message end");
+            }
+        }
+    }
+}
