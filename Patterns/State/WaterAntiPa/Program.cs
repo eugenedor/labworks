45411b2fc0305c2498559f872bb8f14a65681417
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-02-20 21:47:53 +0300

    Patterns WaterAntiPa State

---
+++ Patterns/State/WaterAntiPa/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterAntiPa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaterAntiPa water = new WaterAntiPa(WaterState.LIQUID);
+            water.Heat();
+            water.Frost();
+            water.Frost();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Patterns/State/WaterAntiPa/WaterAntiPa.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterAntiPa
+{
+    class WaterAntiPa
+    {
+        public WaterState State { get; set; }
+
+        public WaterAntiPa(WaterState ws)
+        {
+            State = ws;
+        }
+
+        public void Heat()
+        {
+            if (State == WaterState.SOLID)
+            {
+                Console.WriteLine("Превращаем лед в жидкость");
+                State = WaterState.LIQUID;
+            }
+            else if (State == WaterState.LIQUID)
+            {
+                Console.WriteLine("Превращаем жидкость в пар");
+                State = WaterState.GAS;
+            }
+            else if (State == WaterState.GAS)
+            {
+                Console.WriteLine("Повышаем температуру водяного пара");
+            }
+        }
+        public void Frost()
+        {
+            if (State == WaterState.SOLID)
+            {
+                Console.WriteLine("Продолжаем заморозку льда");
+            }
+            else if(State == WaterState.LIQUID)
+            {
+                Console.WriteLine("Превращаем жидкость в лед");
+                State = WaterState.SOLID;
+            }
+            else if (State == WaterState.GAS)
+            {
+                Console.WriteLine("Превращаем водяной пар в жидкость");
+                State = WaterState.LIQUID;
+            }
+        }
+    }
+}
