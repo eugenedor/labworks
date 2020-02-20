9e423b0e14a11a8b5890ed658a7521c69344283a
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-02-20 22:02:49 +0300

    Factory Method

--- Patterns/Factory/Factory/Program.cs
+++ Patterns/Factory/Factory/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Factory
+namespace Factory //Factory Method
 {
     class Program
     {
@@ -22,3 +22,28 @@ namespace Factory
         }
     }
 }
+
+
+//abstract class Product
+//{ }
+
+//class ConcreteProductA : Product
+//{ }
+
+//class ConcreteProductB : Product
+//{ }
+
+//abstract class Creator
+//{
+//    public abstract Product FactoryMethod();
+//}
+
+//class ConcreteCreatorA : Creator
+//{
+//    public override Product FactoryMethod() { return new ConcreteProductA(); }
+//}
+
+//class ConcreteCreatorB : Creator
+//{
+//    public override Product FactoryMethod() { return new ConcreteProductB(); }
+//}
