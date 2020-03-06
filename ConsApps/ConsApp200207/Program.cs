98855522e88d64065110f6cb8e0e8a6242666da9
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-03-06 19:08:54 +0300

    ConsApp200207

---
+++ ConsApps/ConsApp200207/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsApp200207
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A aa = new A();
+            A ab = new B();
+            B bb = new B();
+
+            aa.Print();
+            ab.Print();
+            bb.Print();
+            Console.ReadKey();
+        }
+    }
+
+    public interface I
+    {
+        void Print();
+    }
+
+    public class A : I
+    {
+        public void Print()
+        {
+            Method();
+        }
+
+        public virtual void Method()
+        {
+            Console.WriteLine("Method_A");
+        }
+    }
+
+    public class B : A
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Method_B");
+        }
+    }
+}
