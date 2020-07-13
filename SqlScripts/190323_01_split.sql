b3d2c58653cce3792039a72857f60bb6792a1bd7
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-07-13 13:55:55 +0300

    split modify

--- SqlScripts/190323_01_split.sql
+++ SqlScripts/190323_01_split.sql
@@ -1,9 +1,9 @@
 DECLARE @String     NVARCHAR(4000),
-        @Delimiter  NVARCHAR(5),
+        @Delimiter  NVARCHAR(1),
 	    @WithDouble BIT,
         @Pos INT;
 
-DECLARE @SplittedValues TABLE
+DECLARE @tValues TABLE
 (
     Id      INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	Value   NVARCHAR(100) 
@@ -20,20 +20,20 @@ BEGIN
 						WHEN 0 THEN LEN(@String) 
 						ELSE CHARINDEX(@Delimiter, @String) - 1
 					END;
-
+        
     IF (@WithDouble = 1 OR
 		NOT EXISTS(SELECT Value 
-		           FROM @SplittedValues 
-			       WHERE Value = (SELECT CAST(SUBSTRING(@String, 1, @Pos) AS NVARCHAR(100))))
+		            FROM @tValues 
+			     	WHERE Value = (SELECT CAST(SUBSTRING(@String, 1, @Pos) AS NVARCHAR(100))))
 		)
-		INSERT INTO @SplittedValues (Value)  
-		SELECT CAST(SUBSTRING(@String, 1, @Pos) AS NVARCHAR(100))
+		INSERT INTO @tValues (Value)  
+		SELECT LTRIM(RTRIM(CAST(SUBSTRING(@String, 1, @Pos) AS NVARCHAR(MAX))))
 
     SELECT @String = CASE 
-		                    WHEN @Pos != LEN(@String) THEN RIGHT(@String, LEN(@String) - @Pos - 1)
-                            ELSE ''
-					    END
+		               WHEN @Pos != LEN(@String) THEN RIGHT(@String, LEN(@String) - @Pos - 1)
+                       ELSE ''
+					 END
 END 
 
 SELECT * 
-FROM @SplittedValues;
+FROM @tValues;
