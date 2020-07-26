1f7f83a9dc096a55bff2e7ab5becddebeb70737d
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-07-26 23:12:37 +0300

    SampleAppGridView

---
+++ LabClientServerApp/SampleApplication/SampleApplication/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SampleApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListOfConnectionTest = new System.Windows.Forms.ListBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListOfConnectionTest
+            // 
+            this.ListOfConnectionTest.FormattingEnabled = true;
+            this.ListOfConnectionTest.Location = new System.Drawing.Point(12, 12);
+            this.ListOfConnectionTest.Name = "ListOfConnectionTest";
+            this.ListOfConnectionTest.Size = new System.Drawing.Size(260, 95);
+            this.ListOfConnectionTest.TabIndex = 2;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(12, 120);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(260, 23);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Выполнить";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 156);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.ListOfConnectionTest);
+            this.Name = "Form1";
+            this.Text = "Проверка соединения";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListOfConnectionTest;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
