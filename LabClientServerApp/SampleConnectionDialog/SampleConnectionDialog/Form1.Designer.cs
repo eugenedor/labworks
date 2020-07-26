ff967900525eb0c36a88aececdbcd7222db0a9b1
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-07-26 23:30:38 +0300

    SampleConnectionDialog

---
+++ LabClientServerApp/SampleConnectionDialog/SampleConnectionDialog/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SampleConnectionDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.btnConnection = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtString
+            // 
+            this.txtString.Location = new System.Drawing.Point(12, 25);
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(618, 20);
+            this.txtString.TabIndex = 2;
+            // 
+            // btnConnection
+            // 
+            this.btnConnection.Location = new System.Drawing.Point(447, 68);
+            this.btnConnection.Name = "btnConnection";
+            this.btnConnection.Size = new System.Drawing.Size(183, 23);
+            this.btnConnection.TabIndex = 3;
+            this.btnConnection.Text = "Выбрать соединение";
+            this.btnConnection.UseVisualStyleBackColor = true;
+            this.btnConnection.Click += new System.EventHandler(this.btnConnection_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 103);
+            this.Controls.Add(this.btnConnection);
+            this.Controls.Add(this.txtString);
+            this.Name = "Form1";
+            this.Text = "Форма с вызовом диалога настройки соединения";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.Button btnConnection;
+    }
+}
+
