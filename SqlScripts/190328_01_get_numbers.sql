9c3e6a0fce4f11da254cf82fcce7bda2beeda909
Eugene Dorokhin <eugenedor1986@gmail.com>
2020-08-05 12:28:20 +0300

    190328_01_get_numbers modify

--- SqlScripts/190328_01_get_numbers.sql
+++ SqlScripts/190328_01_get_numbers.sql
@@ -4,6 +4,7 @@ DECLARE @From INT,
 SET @From = 1;
 SET @To = 777;
 
+--1--
 WITH t AS (SELECT n 
 	        FROM (VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9)) v(n))
 	
@@ -17,3 +18,19 @@ FROM (SELECT 1 + t1.n + t2.n*10 + t3.n*100 + t4.n*1000 + t5.n*10000 + t6.n*10000
 		   t t6) tbl
 WHERE tbl.n BETWEEN @From AND @To
 ORDER BY tbl.n;
+
+--2--
+WITH e1(n) AS
+	(
+		SELECT 1 UNION ALL SELECT 1 UNION ALL SELECT 1 UNION ALL 
+		SELECT 1 UNION ALL SELECT 1 UNION ALL SELECT 1 UNION ALL 
+		SELECT 1 UNION ALL SELECT 1 UNION ALL SELECT 1 UNION ALL SELECT 1
+	), -- 10
+	e2(n) AS (SELECT 1 FROM e1 CROSS JOIN e1 AS b), -- 10*10
+	e3(n) AS (SELECT 1 FROM e1 CROSS JOIN e2), -- 10*100
+	e4(n) AS (SELECT 1 from e1 CROSS JOIN e3) -- 10*1000
+
+SELECT * 
+FROM (SELECT n = ROW_NUMBER() OVER (ORDER BY n) 
+      FROM e4) q 
+WHERE q.n between @From and @To ORDER BY n;
