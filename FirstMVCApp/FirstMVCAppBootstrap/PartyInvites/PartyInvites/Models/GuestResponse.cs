e323ebe67d6cf4329a3860a3f0737fcf20902c71
Eugene Dorokhin <eugenedor1986@gmail.com>
2021-10-24 16:07:07 +0300

    PartyInvites

---
+++ FirstMVCApp/FirstMVCAppBootstrap/PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please indicate whether to take part in a party")]
+        public bool? WillAttend { get; set; }
+    }
+}
