4358848470360b05d482994941aad94008117bef
eugenedor1986 <eugenedor1986@gmail.com>
2022-02-13 23:05:01 +0300

    StrParseVal modify

--- StrParseVal/StrParseVal/Program.cs
+++ StrParseVal/StrParseVal/Program.cs
@@ -14,36 +14,36 @@ namespace StrParseVal
         {
             string[] strs = new string[]
             {
-                "2012-03-14 22:35:31.027",
-                "14/03/2012",
-                "14 марта 2012 г.",
-                "14/03/12",
-                "14 мар 19",
-                "14-мар-2012",
-                "2012-03-14",
-                "14-Mar-2012",
-                "03/14/2012",
-                "03/14/12",
-                "2012.03.14",
-                "12.03.14",
-                "14.03.12",
-                "14.03.2012",
-                "0.00",
-                "0,00",
-                "123.499989",
-                "567,999989",
-                "Какой-то текст 14.03.2012 123.45 987,65!",
-                "2147483647",
-                "-2147483647",
-                "2147483648",
-                "-2147483648",
-                "9223372036854775807",
-                "007",
-                "00008",
-                "a000009",
-                "FALse",
-                "TRue",
-                ""
+                //"2012-03-14 22:35:31.027",
+                //"14/03/2012",
+                //"14 марта 2012 г.",
+                //"14/03/12",
+                //"14 мар 19",
+                //"14-мар-2012",
+                //"2012-03-14",
+                //"14-Mar-2012",
+                //"03/14/2012",
+                //"03/14/12",
+                //"2012.03.14",
+                //"12.03.14",
+                //"14.03.12",
+                //"14.03.2012",
+                //"0.00",
+                //"0,00",
+                "123.49",
+                "567,89"//,
+                //"Какой-то текст 14.03.2012 123.45 987,65!",
+                //"2147483647",
+                //"-2147483647",
+                //"2147483648",
+                //"-2147483648",
+                //"9223372036854775807",
+                //"007",
+                //"00008",
+                //"a000009",
+                //"FALse",
+                //"TRue",
+                //""
             };
 
             int i = 0;
@@ -73,35 +73,35 @@ namespace StrParseVal
                     return value;
                 }
 
-                var cultIn = CultureInfo.InvariantCulture;
+                if (Regex.IsMatch(value, @"^\d+$") && value.StartsWith("0"))
+                {
+                    return value;
+                }
+                if (long.TryParse(value, out long longResult))
+                {
+                    return longResult.ToString();
+                }
+
+                if (Regex.IsMatch(value, @"^[-]?\d+[,]\d+$"))
+                {
+                    return value.Replace(",", ".");
+                }
+                if (Regex.IsMatch(value, @"^[-]?\d+[.]\d+$"))
+                {
+                    return value;
+                }
+                if (decimal.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture, out decimal decResult))
+                {
+                    return decResult.ToString();
+                }
+
                 var cultRu = CultureInfo.CreateSpecificCulture("ru-Ru");
                 var cultEn = CultureInfo.CreateSpecificCulture("en-US");
-
                 var dateStyles = DateTimeStyles.None;
                 if (DateTime.TryParse(value, cultRu, dateStyles, out DateTime dateResult) ||
                     DateTime.TryParse(value, cultEn, dateStyles, out dateResult))
                 {
                     return dateResult.ToString("dd.MM.yyyy");
-                }
-
-                if (Regex.IsMatch(value, @"^\d+$") && value.StartsWith("0"))
-                {
-                    return value;
-                }
-
-                var numStylesInt = NumberStyles.Integer;
-                if (long.TryParse(value, numStylesInt, cultIn, out long longResult))
-                {
-                    return longResult.ToString();
-                }
-
-                var numStylesNum = NumberStyles.Number;
-                var numStylesFlt = NumberStyles.Float;
-                if (decimal.TryParse(value, numStylesNum, cultRu, out decimal decResult) ||
-                    decimal.TryParse(value, numStylesNum, cultEn, out decResult) ||
-                    decimal.TryParse(value, numStylesFlt, cultIn, out decResult))
-                {
-                    return decResult.ToString(cultEn);
                 }
 
                 if (bool.TryParse(value, out bool boolResult))
