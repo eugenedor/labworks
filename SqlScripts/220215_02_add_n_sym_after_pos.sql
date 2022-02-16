c9e931ddea926244f23523b40a2d84146805ba05
eugenedor1986 <eugenedor1986@gmail.com>
2022-02-16 12:24:55 +0300

    220215_02_add_n_sym_after_pos modify

--- SqlScripts/220215_02_add_n_sym_after_pos.sql
+++ SqlScripts/220215_02_add_n_sym_after_pos.sql
@@ -8,33 +8,42 @@ SET @pos = 15;
 SET @pad = '0';
 SELECT @pos pos, @pad pad;
 
-
-SET @string = '123456789012345';
-SET @diff = CASE WHEN LEN(@string) - @pos > 0 THEN LEN(@string) - @pos ELSE 0 END;
-SELECT 1 num, @string string, @diff diff, REPLICATE(@pad, @diff) repl,         SUBSTRING(@string, 1, @pos) + REPLICATE(@pad, @diff) string_modify UNION ALL
-SELECT 2 num, @string string, @diff diff, REPLICATE(@pad, LEN(@string)) repl,  SUBSTRING(@string, 1, @pos) + SUBSTRING(REPLICATE(@pad, LEN(@string)), 1, @diff) AS string_modify;
-
 SET @string = '';
 SET @diff = CASE WHEN LEN(@string) - @pos > 0 THEN LEN(@string) - @pos ELSE 0 END;
 SELECT 1 num, @string string, @diff diff, REPLICATE(@pad, @diff) repl,         SUBSTRING(@string, 1, @pos) + REPLICATE(@pad, @diff) string_modify UNION ALL
 SELECT 2 num, @string string, @diff diff, REPLICATE(@pad, LEN(@string)) repl,  SUBSTRING(@string, 1, @pos) + SUBSTRING(REPLICATE(@pad, LEN(@string)), 1, @diff) AS string_modify;
 
-SET @string = '123456789';
+SET @string = '12345678901';
 SET @diff = CASE WHEN LEN(@string) - @pos > 0 THEN LEN(@string) - @pos ELSE 0 END;
 SELECT 1 num, @string string, @diff diff, REPLICATE(@pad, @diff) repl,         SUBSTRING(@string, 1, @pos) + REPLICATE(@pad, @diff) string_modify UNION ALL
 SELECT 2 num, @string string, @diff diff, REPLICATE(@pad, LEN(@string)) repl,  SUBSTRING(@string, 1, @pos) + SUBSTRING(REPLICATE(@pad, LEN(@string)), 1, @diff) AS string_modify;
 
-SET @string = '1234567890123456';
+SET @string = '1234567890123';
 SET @diff = CASE WHEN LEN(@string) - @pos > 0 THEN LEN(@string) - @pos ELSE 0 END;
 SELECT 1 num, @string string, @diff diff, REPLICATE(@pad, @diff) repl,         SUBSTRING(@string, 1, @pos) + REPLICATE(@pad, @diff) string_modify UNION ALL
 SELECT 2 num, @string string, @diff diff, REPLICATE(@pad, LEN(@string)) repl,  SUBSTRING(@string, 1, @pos) + SUBSTRING(REPLICATE(@pad, LEN(@string)), 1, @diff) AS string_modify;
 
-SET @string = '1234567890123456789';
+SET @string = '12345678901234';
+SET @diff = CASE WHEN LEN(@string) - @pos > 0 THEN LEN(@string) - @pos ELSE 0 END;
+SELECT 1 num, @string string, @diff diff, REPLICATE(@pad, @diff) repl,         SUBSTRING(@string, 1, @pos) + REPLICATE(@pad, @diff) string_modify UNION ALL
+SELECT 2 num, @string string, @diff diff, REPLICATE(@pad, LEN(@string)) repl,  SUBSTRING(@string, 1, @pos) + SUBSTRING(REPLICATE(@pad, LEN(@string)), 1, @diff) AS string_modify;
+
+SET @string = '123456789012345';
 SET @diff = CASE WHEN LEN(@string) - @pos > 0 THEN LEN(@string) - @pos ELSE 0 END;
 SELECT 1 num, @string string, @diff diff, REPLICATE(@pad, @diff) repl,         SUBSTRING(@string, 1, @pos) + REPLICATE(@pad, @diff) string_modify  UNION ALL
 SELECT 2 num, @string string, @diff diff, REPLICATE(@pad, LEN(@string)) repl,  SUBSTRING(@string, 1, @pos) + SUBSTRING(REPLICATE(@pad, LEN(@string)), 1, @diff) AS string_modify;
 
-SET @string = '1234567890123456789012';
+SET @string = '1234567890123456';
+SET @diff = CASE WHEN LEN(@string) - @pos > 0 THEN LEN(@string) - @pos ELSE 0 END;
+SELECT 1 num, @string string, @diff diff, REPLICATE(@pad, @diff) repl,         SUBSTRING(@string, 1, @pos) + REPLICATE(@pad, @diff) string_modify  UNION ALL
+SELECT 2 num, @string string, @diff diff, REPLICATE(@pad, LEN(@string)) repl,  SUBSTRING(@string, 1, @pos) + SUBSTRING(REPLICATE(@pad, LEN(@string)), 1, @diff) AS string_modify;
+
+SET @string = '12345678901234567';
+SET @diff = CASE WHEN LEN(@string) - @pos > 0 THEN LEN(@string) - @pos ELSE 0 END;
+SELECT 1 num, @string string, @diff diff, REPLICATE(@pad, @diff) repl,         SUBSTRING(@string, 1, @pos) + REPLICATE(@pad, @diff) string_modify  UNION ALL
+SELECT 2 num, @string string, @diff diff, REPLICATE(@pad, LEN(@string)) repl,  SUBSTRING(@string, 1, @pos) + SUBSTRING(REPLICATE(@pad, LEN(@string)), 1, @diff) AS string_modify;
+
+SET @string = '1234567890123456789';
 SET @diff = CASE WHEN LEN(@string) - @pos > 0 THEN LEN(@string) - @pos ELSE 0 END;
 SELECT 1 num, @string string, @diff diff, REPLICATE(@pad, @diff) repl,         SUBSTRING(@string, 1, @pos) + REPLICATE(@pad, @diff) string_modify  UNION ALL
 SELECT 2 num, @string string, @diff diff, REPLICATE(@pad, LEN(@string)) repl,  SUBSTRING(@string, 1, @pos) + SUBSTRING(REPLICATE(@pad, LEN(@string)), 1, @diff) AS string_modify;
