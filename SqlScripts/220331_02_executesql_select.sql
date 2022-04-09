f9bdd34e664f215dd92cf145d8db4719ac3793f2
eugenedor1986 <eugenedor1986@gmail.com>
2022-04-09 22:31:23 +0300

    220331_02_executesql_select

--- SqlScripts/220331_02_executesql_select.sql
+++ SqlScripts/220331_02_executesql_select.sql
@@ -1,6 +1,10 @@
 DECLARE @table TABLE (rnum BIGINT, val VARCHAR(50));
 
-INSERT INTO @table (rnum, val) VALUES (1,'Наименование'), (2, 'Код'), (3, 'Значение');
+INSERT INTO @table (rnum, val) 
+SELECT 1 AS rnum,'Наименование' AS val UNION ALL
+SELECT 2, 'Код' UNION ALL
+SELECT 3, 'Значение'
+ORDER BY rnum;
 
 SELECT t.rnum, t.val 
 FROM @table t 
@@ -10,9 +14,9 @@ DECLARE @query NVARCHAR(MAX);
 WITH tcnt AS (SELECT COUNT(t0.rnum) countRows
 			  FROM @table t0)
 SELECT @query = ISNULL(@query, 'SELECT ') + 
-				'''' + t.val + ''' AS N''' + CAST(t.rnum AS NVARCHAR(25)) + '''' +
+				'''' + t.val + ''' AS clmn' + CAST(t.rnum AS NVARCHAR(25)) +
 				CASE 
-				  WHEN ROW_NUMBER() OVER (ORDER BY t.rnum ASC) = tcnt.countRows THEN ';' 
+				  WHEN t.rnum  = tcnt.countRows THEN ';' 
 				  ELSE ',' 
 				END + ' '
 FROM @table t 
