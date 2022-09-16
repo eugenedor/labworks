2ecc82d617c6785babf5721894417c9edf5cdbfe
eugenedor1986 <eugenedor1986@gmail.com>
2022-09-16 13:48:02 +0300

    DecParseAnalysis modify for test (Best variant where: AllowDecimalPoint and comma -> dot)

--- DecParseAnalysis/DecParseAnalysis/Program.cs
+++ DecParseAnalysis/DecParseAnalysis/Program.cs
@@ -14,18 +14,20 @@ namespace DecParseAnalysis
             var str1 = "123.456";
             var str2 = "987,654";
 
-            Console.WriteLine(12.34m.ToString("F8", new CultureInfo("ru-RU")));
-            Console.WriteLine(12.34m.ToString("F15", new CultureInfo("ru-RU")));
-            Console.WriteLine();
+            //Console.WriteLine(12.34m.ToString("F8", new CultureInfo("ru-RU")));
+            //Console.WriteLine(12.34m.ToString("F15", new CultureInfo("ru-RU")));
+            //Console.WriteLine();
 
             Console.WriteLine($"str1 = {str1}");
             Console.WriteLine($"str2 = {str2}");
 
             Console.WriteLine();
+            Console.WriteLine("MetOne");
             MetOne(str1);
             MetOne(str2);
 
             Console.WriteLine();
+            Console.WriteLine("MetMod");
             MetMod(str1);
             MetMod(str2);
 
@@ -56,7 +58,12 @@ namespace DecParseAnalysis
                 Console.WriteLine("comma -> dot");
                 if (str.Contains(","))
                     str = str.Replace(",", ".");
-                //str = SymReplace(str, comma, dot);
+
+                //Console.WriteLine("dot -> comma");
+                //if (str.Contains("."))
+                //    str = str.Replace(".", ",");
+
+                //str = SymReplace(str, comma, dot)
                 //Console.WriteLine("dot -> comma");
                 //str = SymReplace(str, dot, comma);
                 Console.WriteLine($"{nameof(str)} = {str}");
@@ -83,6 +90,9 @@ namespace DecParseAnalysis
             {
                 if (str.Contains(","))
                     str = str.Replace(",", ".");
+
+                //if (str.Contains("."))
+                //    str = str.Replace(".", ",");
 
                 if (decimal.TryParse(str, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out dec))
                 {
