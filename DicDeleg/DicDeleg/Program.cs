bc76defad5b8b651695afec7fa0d1a70e1e3bcc7
eugenedor1986 <eugenedor1986@gmail.com>
2023-04-15 18:59:09 +0300

    DicDeleg

---
+++ DicDeleg/DicDeleg/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicDeleg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+
+            var v1 = 9m;
+            var v2 = 3m;
+
+            Console.WriteLine("*****SimpleOperation*****");
+            Console.WriteLine("variables1:");
+            Console.WriteLine($"v1 = {v1}; v2 = {v2}");
+            Console.WriteLine();
+
+            Console.WriteLine($"result1(+) = {calc.SimpleOperation("+", v1, v2)}");
+            Console.WriteLine($"result1(-) = {calc.SimpleOperation("-", v1, v2)}");
+            Console.WriteLine($"result1(*) = {calc.SimpleOperation("*", v1, v2)}");
+            Console.WriteLine($"result1(/) = {calc.SimpleOperation("/", v1, v2)}");
+            Console.WriteLine();
+
+            v1 = 14m;
+            v2 = 2m;
+
+            Console.WriteLine("*****PerformOperation*****");
+            Console.WriteLine("variables2:");
+            Console.WriteLine($"v1 = {v1}; v2 = {v2}");
+            Console.WriteLine();
+
+            Console.WriteLine($"result2(+) = {calc.PerformOperation("+", v1, v2)}");
+            Console.WriteLine($"result2(-) = {calc.PerformOperation("-", v1, v2)}");
+            Console.WriteLine($"result2(*) = {calc.PerformOperation("*", v1, v2)}");
+            Console.WriteLine($"result2(/) = {calc.PerformOperation("/", v1, v2)}");
+
+            Console.WriteLine("defineoperation: mod %");
+            calc.DefineOperation("mod", (x, y) => x % y);
+            Console.WriteLine($"result2(mod) = {calc.PerformOperation("mod", v1, v2)}");
+
+            Console.WriteLine(System.Environment.NewLine + "Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
