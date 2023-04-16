262856e32162b778955889d36d0faa8ede702d82
eugenedor1986 <eugenedor1986@gmail.com>
2023-04-16 23:44:29 +0300

    algohistory

--- SqlScripts/230416_01_algohistory.sql
+++ SqlScripts/230416_01_algohistory.sql
@@ -1,41 +1,51 @@
-DECLARE @t TABLE (Id INT IDENTITY(1,1), HistoryParentId INT, HistoryState INT, ModifiedDate DATETIME);
+CREATE TABLE #t (
+Id INT IDENTITY(1,1), 
+HistoryParentId INT, 
+HistoryState INT, 
+ModifiedDate DATETIME);
 
-INSERT INTO @t (HistoryParentId, HistoryState, ModifiedDate) VALUES (0, 3, GETDATE());
+SET IDENTITY_INSERT #t ON;
+
+INSERT INTO #t (Id, HistoryParentId, HistoryState, ModifiedDate) VALUES (1, 0, 2, GETDATE());
 WAITFOR DELAY '00:00:01' 
-INSERT INTO @t (HistoryParentId, HistoryState, ModifiedDate) VALUES (0, 3, GETDATE());
-WAITFOR DELAY '00:00:03' 
-INSERT INTO @t (HistoryParentId, HistoryState, ModifiedDate) VALUES (0, 3, GETDATE());
-WAITFOR DELAY '00:00:02' 
-INSERT INTO @t (HistoryParentId, HistoryState, ModifiedDate) VALUES (0, 3, GETDATE());
+INSERT INTO #t (Id, HistoryParentId, HistoryState, ModifiedDate) VALUES (2, 0, 2, GETDATE());
+WAITFOR DELAY '00:00:04' 
+INSERT INTO #t (Id, HistoryParentId, HistoryState, ModifiedDate) VALUES (3, 0, 4, GETDATE());
+INSERT INTO #t (Id, HistoryParentId, HistoryState, ModifiedDate) VALUES (4, 3, 3, DATEADD(ss, -1, GETDATE()));
 
-SELECT * FROM @t;
+SET IDENTITY_INSERT #t OFF;
+
+SELECT * FROM #t ORDER BY Id;;
 
 DECLARE @newID TABLE (Id INT);
 DECLARE @Id INT;
 
-SET @Id = 2;
+SET @Id = 3;
 
-INSERT INTO @t 
+INSERT INTO #t 
 OUTPUT INSERTED.Id INTO @newID
 SELECT HistoryParentId, HistoryState, ModifiedDate
-FROM @t
+FROM #t
 WHERE Id = @Id;
 
-UPDATE @t
+UPDATE #t
 SET HistoryParentId = @Id
 WHERE Id = (SELECT TOP 1 Id FROM @newID);
 
-UPDATE @t
+UPDATE #t
 SET HistoryParentId = (SELECT TOP 1 Id FROM @newID)
 WHERE HistoryParentId = @Id
       AND Id != (SELECT TOP 1 Id FROM @newID)
 
 
-WAITFOR DELAY '00:00:05' 
-UPDATE @t
+WAITFOR DELAY '00:00:03' 
+UPDATE #t
 SET HistoryState = 4,
 	ModifiedDate = GETDATE()
 WHERE Id = @Id
 
 
-SELECT * FROM @t;
+SELECT * FROM #t ORDER BY ModifiedDate DESC;
+
+DROP TABLE #t;
+GO
