1970d5091f79f85f307ef0fcbd1efc41ad0d48e2
eugenedor1986 <eugenedor1986@gmail.com>
2023-04-26 10:59:36 +0300

    CrossingPeriods

---
+++ CrossingPeriods/CrossingPeriods/DateTimePeriod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossingPeriods
+{
+    public struct DateTimePeriod
+    {
+        public DateTime? Start { get; }
+        public DateTime? End { get; }
+
+        public DateTimePeriod(DateTime? start, DateTime? end)
+        {
+            if (start.HasValue && start.HasValue)
+                if (start.Value > end.Value)
+                    throw new Exception($"Дата начала диапазона {start.Value} старше конечной даты {end.Value}");
+
+            Start = start;
+            End = end;
+        }
+
+        public bool IsIntersected(DateTimePeriod period2)
+        {
+            var start1 = Start ?? DateTime.MinValue;
+            var end1 = End ?? DateTime.MaxValue;
+            var start2 = period2.Start ?? DateTime.MinValue;
+            var end2 = period2.End ?? DateTime.MaxValue;
+
+            //var start = new DateTime(Math.Max(start1.Ticks, start2.Ticks));
+            //var end = new DateTime(Math.Min(end1.Ticks, end2.Ticks));
+
+            var start = start1 > start2 ? start1 : start2;
+            var end = end1 < end2 ? end1 : end2;
+
+            return start <= end;
+        }
+    }
+}
---
+++ CrossingPeriods/CrossingPeriods/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossingPeriods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = new DateTime(2023, 6, 6);
+            var end = new DateTime(2023, 7, 7);
+
+            var filterPeriod = new DateTimePeriod(start, end);
+            Console.Write($"Period: {filterPeriod.Start?.Date:yyyy.MM.dd} - {filterPeriod.End?.Date:yyyy.MM.dd} ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var periods = new Dictionary<int, DateTimePeriod>
+            {
+                [1] = new DateTimePeriod(new DateTime(2023, 1, 1), new DateTime(2023, 3, 31)),
+                [2] = new DateTimePeriod(new DateTime(2023, 4, 1), new DateTime(2023, 6, 30)),
+                [3] = new DateTimePeriod(new DateTime(2023, 7, 1), new DateTime(2023, 9, 30)),
+                [4] = new DateTimePeriod(new DateTime(2023, 10, 1), new DateTime(2023, 12, 31))
+            };
+
+            periods[14] = new DateTimePeriod(new DateTime(2023, 4, 1), new DateTime(2023, 4, 30));
+            periods[15] = new DateTimePeriod(new DateTime(2023, 5, 1), new DateTime(2023, 5, 31));
+            periods[16] = new DateTimePeriod(new DateTime(2023, 6, 1), new DateTime(2023, 6, 30));
+            periods[17] = new DateTimePeriod(new DateTime(2023, 7, 1), new DateTime(2023, 7, 31));
+            periods[18] = new DateTimePeriod(new DateTime(2023, 8, 1), new DateTime(2023, 8, 31));
+            periods[19] = new DateTimePeriod(new DateTime(2023, 9, 1), new DateTime(2023, 9, 30));
+
+            foreach (var period in periods)
+            {
+                Console.Write($"Period_{period.Key} ({period.Value.Start?.Date:yyyy.MM.dd} - {period.Value.End?.Date:yyyy.MM.dd}) is intersected: ");
+                Console.Write(period.Value.IsIntersected(filterPeriod));
+                Console.WriteLine($"  {Convert.ToInt32(period.Value.IsIntersected(filterPeriod))}");
+            }
+
+            Console.WriteLine(System.Environment.NewLine + "Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
