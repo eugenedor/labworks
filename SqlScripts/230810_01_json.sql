d9422c7b7d2d1d856bf2029cde70a74bb13b25c9
eugenedor1986 <eugenedor1986@gmail.com>
2023-08-11 00:34:35 +0300

    SqlScript json modify

--- SqlScripts/230810_01_json.sql
+++ SqlScripts/230810_01_json.sql
@@ -14,7 +14,8 @@ SELECT @json;
 --Unpacking--
 SET @json = '[{"clmnKey":0,"clmnName":"a"},{"clmnKey":1,"clmnName":"b"},{"clmnKey":2,"clmnName":"c"},{"clmnKey":3,"clmnName":"d"}]';
 
-SELECT * FROM OPENJSON(@json) WITH (
+SELECT t.columnKey, t.columnName
+FROM OPENJSON(@json) WITH (
 columnKey INT '$.clmnKey',
 columnName VARCHAR(100) '$.clmnName'
-);
+) t;
