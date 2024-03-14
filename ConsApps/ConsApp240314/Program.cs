b5fdd9b8eadd2c279e337d391e038b0b5e3375c0
eugenedor1986 <eugenedor1986@gmail.com>
2024-03-14 13:34:08 +0300

    ConsApp240314

---
+++ ConsApps/ConsApp240314/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsApp240314
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new string[]
+{
+                "",
+                ",",
+                "000000",
+                "000000,",
+                "000000,101000",
+                "000000,101000,",
+                "000000,101000,202000",
+                "000000,101000,202000,",
+            };
+
+
+            int i = 0;
+            foreach (string item in items)
+            {
+                var codes = new List<string>() { "000000", "101000", "202000", "303000", "404000", "505000" };
+
+                Console.WriteLine($"--{i}--------------------------------------------------");
+                
+                Console.Write($"codes{i}: ");
+                foreach (string code in codes)
+                    Console.Write("[" + code + "]");
+                Console.WriteLine();
+
+                Console.WriteLine($"item{i} = \"{item}\"");
+                Console.WriteLine();
+
+                if (!string.IsNullOrWhiteSpace(item))
+                {
+                    char comma = ',';
+                    if (!DigitsOnlyInString(item.Trim()) && item.IndexOf(comma, 0) != -1)
+                    {
+                        Console.WriteLine($"***Parse***");
+                        //string[] itemArray = item.Split(comma).ToArray();
+                        string[] itemArray = item.Split(comma).Where(p => !string.IsNullOrWhiteSpace(p)).ToArray();
+                        Console.Write($"length={itemArray.Length}   ");
+                        foreach (string it in itemArray)
+                            Console.Write("[" + it + "]");
+                        Console.WriteLine("");
+                        Console.WriteLine($"***********");
+
+                        codes = codes.Where(p => itemArray.Contains(p)).ToList();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"***Equal***");
+                        Console.WriteLine(item);
+                        Console.WriteLine($"***********");
+
+                        codes = codes.Where(p => p == item).ToList();
+                    }
+
+                    Console.WriteLine();
+                    Console.Write("codesResult: ");
+                    foreach (string code in codes)
+                        Console.Write("[" + code + "]");
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("------------------------------------------------------");
+                Console.WriteLine();
+                ++i;
+            }
+
+            Console.WriteLine(System.Environment.NewLine + "Press and key to exit");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Только цифры в строке
+        /// </summary>
+        static bool DigitsOnlyInString(string s)
+        {
+            foreach (char ch in s)
+            {
+                if (!char.IsDigit(ch))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
